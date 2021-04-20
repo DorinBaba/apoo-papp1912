33526c5b980f5718d539cb401f5ee8af6aa422e3
BabaDorin <bvd.dorin@gmail.com>
2021-04-20 11:02:37 +0300

    aptechka practica

---
+++ AptecikaPractica/fisierText/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+
+namespace fisierText
+{
+    //    Creați clasa Elev cu umătoarele:
+    //•	Date: Nume, Prenume, Grupa, Anul de studii, Media generală
+    //Creați o listă în care veți însera cel puțin 2 elevi.
+    //Înscrieți lista rezultată în fișierul date.out
+
+    class Elev
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public int AnulDeStudii { get; set; }
+        public double MediaGenerala { get; set; }
+
+        public override string ToString()
+        {
+            return $"Date despre elev:\n" +
+                $"Nume: {Nume},\n" +
+                $"Prenume: {Prenume}, \n" +
+                $"Anul de studii: {AnulDeStudii},\n" +
+                $"MediaGenerala: {MediaGenerala}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Elev> elevi = new List<Elev>();
+            elevi.Add(new Elev
+            {
+                Nume = "Musk",
+                Prenume = "Elon",
+                AnulDeStudii = 3,
+                MediaGenerala = 9.12
+            });
+
+            elevi.Add(new Elev
+            {
+                Nume = "Buffet",
+                Prenume = "Waren",
+                AnulDeStudii = 2,
+                MediaGenerala = 10
+            });
+
+            StreamWriter sw = new StreamWriter("date.out");
+            foreach(var elev in elevi)
+            {
+                sw.WriteLine(elev);
+                sw.WriteLine();
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ AptecikaPractica/fisierTextCitire/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace fisierTextCitire
+{
+    class Elev
+    {
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Grupa { get; set; }
+        public int AnulDeStudii { get; set; }
+        public double MediaGenerala { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("date.in");
+            List<Elev> elevi = new List<Elev>();
+            for (int i = 0; i < 5; i++)
+            {
+                string[] date = sr.ReadLine().Split("@@@");
+                elevi.Add(new Elev
+                {
+                    Nume = date[0],
+                    Prenume = date[1],
+                    Grupa = date[2],
+                    AnulDeStudii = int.Parse(date[3]),
+                    MediaGenerala = double.Parse(date[4])
+                });
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(elevi[i].Prenume);
+            }
+        }
+    }
+}
